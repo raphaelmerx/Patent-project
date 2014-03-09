46924d55efe429d8ea3325aaa7f0883ea99558e7
raphaelmerx <raphael.merx@berkeley.edu>
2014-03-08 21:20:58 -0800

    new patent document class

--- Blocking data/TSVtoCSV.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('links.tsv','rb') as tsvin, open('links.csv', 'wb') as csvout:
-    tsvin = csv.reader(tsvin, delimiter='\t')
-    csvout = csv.writer(csvout)
-    for row in tsvin:
-        csvout.writerow(row)
--- Data collection/capstone_SQLAlchemy.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import *
-
-db = create_engine('mysql://uspto:ferrisbueller@patent.czb2hytpd5lf.us-west-1.rds.amazonaws.com:3306/uspto')
-
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
